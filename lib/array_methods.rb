09920c594b68bf6246835e733ab6320ee2a7faed
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-07-31 15:37:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,12 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  savePoint = 0
+  array.each do |num|
+    if savePoint < num 
+      savePoint = num 
+    end 
+    savePoint
 end
 
 def find_min_value(array)
