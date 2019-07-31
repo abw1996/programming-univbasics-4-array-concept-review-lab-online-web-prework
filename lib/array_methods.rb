690379139a604c10405fa910fb6093e001c2601e
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-07-31 15:38:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,18 @@ def find_max_value(array)
     if savePoint < num 
       savePoint = num 
     end 
+  end
     savePoint
 end
 
 def find_min_value(array)
-  # Add your solution here
+  savePoint = nil 
+  array.each do |num|
+    if savePoint == nil 
+      savePoint = num 
+    elsif savePoint > num 
+      savePoint = num
+    end
+  end
+  savePoint
 end
